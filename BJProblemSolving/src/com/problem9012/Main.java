fdfa67c30a8f0b86a80453d4e0ad0c532495708b
Tmax <Tmax@DESKTOP-9FN01BS>
2018-05-16 18:55:43 +0900

    20180516-18:55

--- BJProblemSolving/src/com/problem9012/Main.java
+++ BJProblemSolving/src/com/problem9012/Main.java
@@ -1,8 +1,9 @@
 package com.problem9012;
 
 import java.util.Scanner;
+import java.util.Stack;
 
-public class Main {
+/*public class Main {
 	public static void main(String[] args) {
 		String str;
 		Scanner sc = new Scanner(System.in);
@@ -34,4 +35,46 @@ public class Main {
 		else
 			return "NO";
 	}
+}*/
+
+/*
+ * ±¦ÂúÀº ÄÚµå¶ó °¡Á®¿Íº½.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		scanner.nextLine();
+		
+		for(int a=0;a<num;a++) {
+			String input = scanner.nextLine();
+			if(isRight(input)) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+
+	}
+	public static boolean isRight(String input) {
+		boolean isSuccess = true;
+		Stack<Character> stack = new Stack<Character>();
+		try {
+			for(int a=0;a<input.length();a++) {
+				char tmp = input.charAt(a);
+				if(tmp == '(') {
+					stack.push(tmp);
+				}else {
+					stack.pop();
+				}
+			}
+		} catch(Exception e) {
+			isSuccess = false;
+		}
+		
+		return isSuccess && stack.empty();
+	}
+
 }
